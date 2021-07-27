34fd62b709b583f7399c934cc32172a2825f6a1c
April Ding <apr.ding@gmail.com>
2021-07-27 23:30:08 +0800

    initialize sketch 01

---
+++ 01-drawing-snake/main.js
@@ -0,0 +1,13 @@
+/* Exercise from https://wesbos.com beginner js course*/
+
+// select the elements on the page - canvas, shake button
+
+// set up our canvas for drawing
+
+// write a draw function
+
+// write a handler for the keys
+
+// clear function
+
+// listen for arrow keys
